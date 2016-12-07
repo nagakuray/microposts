155f6c2fc8dc2ba229dc6879f9fd738cdb8a7e72
yuuichi.nagakura <nagakuray@nagakurayuuichi-no-MacBook-Air.local>
2016-12-07 21:45:37 +0900

    課題の指摘事項反映.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@ class User < ActiveRecord::Base
   jp_prefecture :birthplace
 
   before_save { self.email = self.email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+	# VALID_BIRTHDAY_REGEX = /\d{4}-\d{2}-\d{2}/
+  validates :birthday, presence: false, length: { maximum: 10 }
+
   has_secure_password
 end
