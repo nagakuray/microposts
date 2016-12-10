0e3bffb4d21ff07787087db49996d48559408977
yuuichi.nagakura <nagakuray@nagakurayuuichi-no-MacBook-Air.local>
2016-12-10 16:03:07 +0900

    db/seeds.rb update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 # coding: utf-8
 
 
-# (101..1000).each do |i|
-# User.create(
-#   name: "seed-#{sprintf("%04d",i)}",
-#   email: "seed-#{sprintf("%04d",i)}@example.com",
-# 	password: "seed-#{sprintf("%04d",i)}",
-# 	blood: "A型",
-# 	birthday: "1986-06-13",
-# )
-# end
-
-# (101..1000).each do |i|
-# 	Relationship.create(
-# 		follower_id: 1,
-# 		followed_id: i
-# 	)
-# end
+(1..1000).each do |i|
+ User.create(
+   name: "seed-#{sprintf("%04d",i)}",
+   email: "seed-#{sprintf("%04d",i)}@example.com",
+ 	password: "seed-#{sprintf("%04d",i)}",
+ 	blood: "A型",
+ 	birthday: "1986-06-13",
+ )
+ end
 
 (101..1000).each do |i|
+	Relationship.create(
+		follower_id: 1,
+		followed_id: i
+	)
+end
+
+(1..1000).each do |i|
 	Micropost.create(
 		user_id: i,
 		content: "テストメッセージ - created by seed-#{sprintf("%04d",i)}"
@@ -27,9 +27,3 @@ end
 
 
 
-#(1001..2000).each do |i|
-#	Micropost.create(
-#		user_id: 1,
-#		content: "あああああああ #{sprintf("%04d",i)}"
-#	)
-#end
