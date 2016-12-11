9038677bb2989d304032a912f83d0f8ae3cd6d9c
yuuichi.nagakura <nagakuray@nagakurayuuichi-no-MacBook-Air.local>
2016-12-11 15:48:58 +0900

    12.2 リツイート機能対応

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   def home
   	if logged_in?
 	  	@micropost = current_user.microposts.build
+	  	# @user = User.find_by(params[:id])
+	  	# @hogehoge = @user.microposts.build
 	  	@feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(25)
 	  end
   end
